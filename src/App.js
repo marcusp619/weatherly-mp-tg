81a779077235a146c8a86115a62d2f7245b4406e
marcusp619 <marcusp619@gmail.com>
2018-09-04 14:11:00 -0600

    Add ignorefile and MockData.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MockData from './MockData'
+
 class App extends Component {
   render() {
     return (
