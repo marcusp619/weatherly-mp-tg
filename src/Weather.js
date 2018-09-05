a6d3bdf12759d6e14ac6bfcb99f4a4fc66802390
Tim Garrity <tag.garrity@gmail.com>
2018-09-05 15:06:18 -0600

    Refactor variables for weather object

--- src/Weather.js
+++ src/Weather.js
@@ -4,24 +4,34 @@ import './Weather.css'
 export default class Weather extends Component {
   constructor(props) {
     super(props)
+
   }
 
   render() {
+    let temperature = this.props.forecast.current_observation.temperature_string;
+    let icon = this.props.forecast.current_observation.icon_url;
+    let weatherCondition = this.props.forecast.current_observation.weather;
+    let city = this.props.forecast.current_observation.display_location.city;
+    let fahrenheit = this.props.forecast.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    let lowTemp = this.props.forecast.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+    let dayForecast = this.props.forecast.forecast.txt_forecast.forecastday[0].fcttext_metric;
+    let nightForecast = this.props.forecast.forecast.txt_forecast.forecastday[1].fcttext_metric;
+
     return (
         <div className='weather'>
-         <h1 className='weather__city'> {this.props.forecast.current_observation.display_location.city}</h1>
+         <h1 className='weather__city'> {city}</h1>
           <h2 className='weather__subtitle'>
-            {this.props.forecast.current_observation.weather}
+            {weatherCondition}
           </h2>
-          <span className="weather__icon"><img src={this.props.forecast.current_observation.icon_url} /></span>
-          <h4>{this.props.forecast.current_observation.temperature_string}</h4>
+          <span className="weather__icon"><img src={icon} /></span>
+          <h4>{temperature}</h4>
           <h4 className='weather__todayHigh'>
-          {this.props.forecast.forecast.simpleforecast.forecastday[0].high.fahrenheit}
+          {fahrenheit}
           </h4>
           <h4 className='weather__todayLow'>
-          {this.props.forecast.forecast.simpleforecast.forecastday[0].low.fahrenheit}
+          {lowTemp}
           </h4>
-          <p className="weather__fullForecast">{this.props.forecast.forecast.txt_forecast.forecastday[0].fcttext_metric} and tonight {this.props.forecast.forecast.txt_forecast.forecastday[1].fcttext_metric} </p>
+          <p className="weather__fullForecast">{dayForecast} and tonight {nightForecast} </p>
         </div>
       )
   }
