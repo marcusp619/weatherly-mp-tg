092b2018ec0259e73c1fafa2807927deb4909fd9
marcusp619 <marcusp619@gmail.com>
2018-09-05 17:04:08 -0600

    Update div to wrap around 7hour

--- src/SevenHour.js
+++ src/SevenHour.js
@@ -13,9 +13,13 @@ export default class SevenHour extends Component {
   render() {
       
     return (
-      this.props.forecast.slice(0, 7).map((hour, index) => {
-        return <Card image={hour.icon_url} time={hour.FCTTIME.civil} temp={hour.temp.english} index={'card' + index}/>
-      })
+      <div>
+        {
+          this.props.forecast.slice(0, 7).map((hour, index) => {
+          return <Card image={hour.icon_url} time={hour.FCTTIME.civil} temp={hour.temp.english} index={'card' + index}/>
+        })
+        }
+      </div>
     )
   }
 }
