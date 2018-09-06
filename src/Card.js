94207a98e44e5aa3dd46c30e05ce8e45030db4c2
marcusp619 <marcusp619@gmail.com>
2018-09-06 17:45:18 -0600

    Create TenDay Component and update card

--- src/Card.js
+++ src/Card.js
@@ -6,10 +6,28 @@ export default class Card extends Component {
   render() {
     return (
       <div className="card" id={this.props.index}>
+        {
+          this.props.weekday &&
+          <h2 className="card__time">{this.props.weekday}</h2>
+        }
+        {
+          this.props.high &&
+          <h2 className="card__temp">{this.props.high}</h2>
+        }
+        {
+          this.props.low &&
+          <h2 className="card__temp">{this.props.low}</h2>
+        }
+        {
+          this.props.time &&
+          <h2 className="card__time">{this.props.time}</h2>
+        }
+        {
+          this.props.temp &&
+          <h2 className="card__temp">{this.props.temp}</h2>
+        }
         <img className="card__icon" src={this.props.image} alt=""/>
-        <h2 className="card__time">{this.props.time}</h2>
-        <h2 className="card__temp">{this.props.temp}</h2>
       </div>
-      )
+    );
   }
 }
