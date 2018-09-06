438887cafc7abfcb995b4e93259571b41672222c
Tim Garrity <tag.garrity@gmail.com>
2018-09-06 13:54:52 -0600

    setup enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should shallow and mount', () => {
+  let wrapper = mount(<App />)
+
+  expect(wrapper).toBeDefined();
+})
